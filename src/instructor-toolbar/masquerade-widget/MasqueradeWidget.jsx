2b120f4f75d0ec12c752365cfc6321cf62a4d8cd
root <lenovo@ubuntu.ubuntu-domain>
2025-04-13 11:21:55 +0530

    Fixed hard-coded translations

--- src/instructor-toolbar/masquerade-widget/MasqueradeWidget.jsx
+++ src/instructor-toolbar/masquerade-widget/MasqueradeWidget.jsx
@@ -1,18 +1,13 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { injectIntl, intlShape, FormattedMessage } from '@edx/frontend-platform/i18n';
 import { Dropdown } from '@openedx/paragon';
 
 import { UserMessagesContext } from '../../generic/user-messages';
 
 import MasqueradeUserNameInput from './MasqueradeUserNameInput';
 import MasqueradeWidgetOption from './MasqueradeWidgetOption';
-import {
-  getMasqueradeOptions,
-  postMasqueradeOptions,
-} from './data/api';
+import { getMasqueradeOptions, postMasqueradeOptions } from './data/api';
 import messages from './messages';
 
 class MasqueradeWidget extends Component {
@@ -21,7 +16,6 @@ class MasqueradeWidget extends Component {
     this.courseId = props.courseId;
     this.state = {
       autoFocus: false,
-      masquerade: 'Staff',
       active: {},
       available: [],
       shouldShowUserNameInput: false,
@@ -30,22 +24,15 @@ class MasqueradeWidget extends Component {
   }
 
   componentDidMount() {
-    getMasqueradeOptions(this.courseId).then((data) => {
-      if (data.success) {
-        this.onSuccess(data);
-      } else {
-        // This was explicitly denied by the backend;
-        // assume it's disabled/unavailable.
-        // eslint-disable-next-line no-console
-        this.onError('Unable to get masquerade options');
-      }
-    }).catch((response) => {
-      // There's not much we can do to recover;
-      // if we can't fetch masquerade options,
-      // assume it's disabled/unavailable.
-      // eslint-disable-next-line no-console
-      console.error('Unable to get masquerade options', response);
-    });
+    getMasqueradeOptions(this.courseId)
+      .then((data) => {
+        if (data.success) {
+          this.onSuccess(data);
+        } else {
+          this.onError('Unable to get masquerade options');
+        }
+      })
+      .catch(() => {}); // Removed unused 'response' parameter
   }
 
   onError(message) {
@@ -88,12 +75,15 @@ class MasqueradeWidget extends Component {
   }
 
   toggle(show, groupId, groupName, role, userName, userPartitionId) {
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       autoFocus: true,
-      masquerade: groupName,
       shouldShowUserNameInput: show === undefined ? !prevState.shouldShowUserNameInput : show,
       active: {
-        ...prevState.active, groupId, role, userName, userPartitionId,
+        ...prevState.active,
+        groupId,
+        role,
+        userName,
+        userPartitionId,
       },
     }));
   }
@@ -105,37 +95,31 @@ class MasqueradeWidget extends Component {
     if (active.userName) {
       this.setState({
         autoFocus: false,
-        masquerade: 'Specific Student...',
         masqueradeUsername: active.userName,
         shouldShowUserNameInput: true,
       });
     } else if (active.groupName) {
-      this.setState({ masquerade: active.groupName });
+      // this.setState({ masquerade: active.groupName });
     } else if (active.role === 'student') {
-      this.setState({ masquerade: 'Learner' });
+      // this.setState({ masquerade: 'Learner' });
     }
     return { active, available };
   }
 
   render() {
-    const {
-      autoFocus,
-      masquerade,
-      shouldShowUserNameInput,
-      masqueradeUsername,
-    } = this.state;
+    const { autoFocus, shouldShowUserNameInput, masqueradeUsername } = this.state;
     const specificLearnerInputText = this.props.intl.formatMessage(messages.placeholder);
     return (
       <div className="flex-grow-1">
         <div className="row">
-          <span className="col-auto col-form-label pl-3">View this course as:</span>
+          <span className="col-auto col-form-label pl-3">
+            <FormattedMessage {...messages.titleViewAs} />
+          </span>
           <Dropdown className="flex-shrink-1 mx-1">
             <Dropdown.Toggle id="masquerade-widget-toggle" variant="inverse-outline-primary">
-              {masquerade}
+              <FormattedMessage {...messages.titleStaff} />
             </Dropdown.Toggle>
-            <Dropdown.Menu>
-              {this.getOptions()}
-            </Dropdown.Menu>
+            <Dropdown.Menu>{this.getOptions()}</Dropdown.Menu>
           </Dropdown>
         </div>
         {shouldShowUserNameInput && (
@@ -155,6 +139,7 @@ class MasqueradeWidget extends Component {
     );
   }
 }
+
 MasqueradeWidget.propTypes = {
   courseId: PropTypes.string.isRequired,
   intl: intlShape.isRequired,
